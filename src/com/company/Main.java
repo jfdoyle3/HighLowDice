3622fb7ae5109dec5848681e655ab58a6f202c53
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2021-07-18 08:11:22 -0400

    game can count consecutives wins

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,28 +5,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        boolean didWin;
+        int winsCount = 0;
+
         Die die1 = new Die();
         Die die2 = new Die();
-        die1.roll();
-        die2.roll();
-        int total = die1.faceUpValue + die2.faceUpValue;
-        boolean didWin = false;
-        int wins=0;
+
         do {
+            didWin = false;
+            die1.roll();
+            die2.roll();
+            int total = die1.faceUpValue + die2.faceUpValue;
+
             System.out.println(die1.faceUpValue + " + " + die2.faceUpValue + " = " + (total));
+
             System.out.println("Will the next number be (h)igher or (l)ower?");
             String input = scanner.next();
 
             die1.roll();
             die2.roll();
             int newTotal = die1.faceUpValue + die2.faceUpValue;
-            if (total == newTotal) {
+
+            if (total == newTotal)
                 System.out.println("same number you lose");
-            }
+
 
             boolean isLarger = newTotal > total;
             System.out.println(die1.faceUpValue + " + " + die2.faceUpValue + " = " + (newTotal));
+
             System.out.println(isLarger ? "Number is higher" : "Number is lower");
 
             if (input.equals("h")) { //guessed higher
@@ -36,14 +44,19 @@ public class Main {
             } else { //guessed lower
                 if (!isLarger) {
                     didWin = true;
-                    wins++;
                 }
             }
-            System.out.println(didWin ? "you WIN" : "You LOSE");
-        }
-        while(didWin);
-        System.out.printf("Consecutive wins: %d",wins);
-        scanner.close();
+            if (didWin) {
+                System.out.println("You Win");
+                winsCount++;
+            } else {
+                System.out.println("You Lose");
+            }
+
+
+        } while (didWin);
+
+        System.out.printf("Consecutive wins: %d", winsCount);
     }
 
 }
