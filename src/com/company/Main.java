b334e7f7e40e0fadc6ce29ccad533f8f7dfdf0db
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2021-07-18 00:40:40 -0400

    brought in die class and code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,49 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        Die die1 = new Die();
+        Die die2 = new Die();
+        die1.roll();
+        die2.roll();
+        int total = die1.faceUpValue + die2.faceUpValue;
+        boolean didWin = false;
+        int wins=0;
+        do {
+            System.out.println(die1.faceUpValue + " + " + die2.faceUpValue + " = " + (total));
+            System.out.println("Will the next number be (h)igher or (l)ower?");
+            String input = scanner.next();
+
+            die1.roll();
+            die2.roll();
+            int newTotal = die1.faceUpValue + die2.faceUpValue;
+            if (total == newTotal) {
+                System.out.println("same number you lose");
+            }
+
+            boolean isLarger = newTotal > total;
+            System.out.println(die1.faceUpValue + " + " + die2.faceUpValue + " = " + (newTotal));
+            System.out.println(isLarger ? "Number is higher" : "Number is lower");
+
+            if (input.equals("h")) { //guessed higher
+                if (isLarger) {
+                    didWin = true;
+                }
+            } else { //guessed lower
+                if (!isLarger) {
+                    didWin = true;
+                    wins++;
+                }
+            }
+            System.out.println(didWin ? "you WIN" : "You LOSE");
+        }
+        while(didWin);
+        System.out.printf("Consecutive wins: %d",wins);
+        scanner.close();
     }
+
 }
